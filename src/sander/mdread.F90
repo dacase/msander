8ccba95295a26adc23992e861d69b4e19570ead5
David A Case <david.case@rutgers.edu>
2019-11-01 16:16:10 -0400

    retain only the middle scheme thermostats

--- src/sander/mdread.F90
+++ src/sander/mdread.F90
@@ -326,9 +326,9 @@ subroutine mdread1()
    nchain = 1
    temp0 = 300.0d0
 ! MIDDLE SCHEME{ 
-   ischeme = 0
+   ischeme = 1
    ithermostat = 0
-   therm_par = 0.0d0
+   therm_par = 5.0d0
 ! } 
 ! PLUMED
    plumed = 0
