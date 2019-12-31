a00cad5f5cccd967259873b48a165dddcee27e6d
David A Case <david.case@rutgers.edu>
2019-12-31 13:47:33 -0500

    in-line the short_ene() subroutine; works -- still slower than original on 1 thread; much slower of multiple threads

--- src/msander/short_ene.F90
+++ src/msander/short_ene.F90
@@ -14,114 +14,12 @@ subroutine get_nb_energy(iac, ico, ntypes, charge, cn1, cn2, cn6, force, &
                          cn4, cn5, epol, dipole, field, mpoltype)
 
   use nblist, only : imagcrds, bckptr, nlogrid, nhigrid, numvdw, numhbnd, &
-                     myindexlo, myindexhi, numimg
-  use constants, only : zero
+                     myindexlo, myindexhi, numimg, tranvec, nucgrd
   use stack
 #ifdef MPI /* SOFT CORE */
   use softcore, only : sc_ener
   use nblist, only : numsc
 #endif
-
-  implicit none
-  character(kind=1, len=13) :: routine="get_nb_energy"
-#ifdef MPI
-#  include "ew_parallel.h"
-#  include "parallel.h"
-#endif
-#include "flocntrl.h"
-#include "def_time.h"
-   
-  integer numatoms, maxnblst, mpoltype
-  integer iac(*), ico(*), ntypes, ee_type, eedmeth
-  _REAL_ charge(*), cn1(*), cn2(*), cn6(*)
-  _REAL_ ewaldcof, eedtbdns, dxdr, eed_cub(4,*), eed_lin(2,*), dir_vir(3,3)
-  integer ipairs(maxnblst)
-  _REAL_ force(3,numatoms), eelt, epol, evdw, ehb
-  _REAL_ eedvir, filter_cut, dipole(3,*), field(3,*), pol(*)
-  _REAL_ pol2(*)
-  _REAL_ cn3(*), cn4(*), cn5(*)
-   
-  integer index, numpack, i, k, ncell_lo, ncell_hi, ntot, nvdw, nhbnd
-  _REAL_ xk, yk, zk
-   
-  if (do_dir == 0) then
-    return
-  end if
-  eelt = zero
-  epol = zero
-  evdw = zero
-  ehb = zero
-  eedvir = zero
-#ifdef MPI /* SOFT CORE */
-  sc_ener(7) = 0.0d0
-  sc_ener(8) = 0.0d0  
-  sc_ener(9) = 0.0d0
-  sc_ener(10) = 0.d0
-  sc_ener(11) = 0.d0
-#endif
-  dir_vir(1:3,1:3) = zero
-  numpack = 1
-  call timer_start(TIME_SHORT_ENE)
-  do index = myindexlo, myindexhi
-    if (numimg(index) > 0) then
-      ncell_lo = nlogrid(index)
-      ncell_hi = nhigrid(index)
-      do k = ncell_lo, ncell_hi
-        i = bckptr(k)
-        xk = imagcrds(1,k)
-        yk = imagcrds(2,k)
-        zk = imagcrds(3,k)
-#ifdef MPI /* SOFT CORE */
-        ! SOFT CORE contribution in numsc
-        ntot = numvdw(i) + numhbnd(i) + numsc(i)
-#else
-        ntot = numvdw(i) + numhbnd(i)
-#endif
-        nvdw = numvdw(i)
-        nhbnd = numhbnd(i)
-        if (ntot > 0) then
-          if (mpoltype == 0) then
-
-            call short_ene(i, xk, yk, zk, ipairs(numpack), ntot, nvdw, nhbnd, &
-                           eedtbdns, eed_cub, eed_lin, charge, ntypes, iac, &
-                           ico, cn1, cn2, cn6, filter_cut, eelt, evdw, force, &
-                           dir_vir, ee_type, eedmeth, dxdr, eedvir, &
-                           cn3, cn4, cn5)
-                        
-          else if ( mpoltype > 0 ) then
-            call short_ene_dip(i, xk, yk, zk, ipairs(numpack), ntot, nvdw, &
-                               ewaldcof, eedtbdns, eed_cub, eed_lin, charge, &
-                               dipole, ntypes, iac, ico, cn1, cn2, cn6, &
-                               filter_cut, eelt, epol, evdw, ehb, force, &
-                               field, pol, pol2, dir_vir, ee_type, eedmeth, &
-                               mpoltype, dxdr, eedvir, cn3, cn4, cn5)
-          end if
-          numpack = numpack + ntot
-        end if  ! ( ntot > 0 )
-      end do  !  k = ncell_lo,ncell_hi
-    end if  ! ( numimg(k) > 0 )
-  end do
-  ! nd loop over this process's assigned atoms
-
-  call timer_stop(TIME_SHORT_ENE)
-
-  return
-
-end subroutine get_nb_energy 
-
-!------------------------------------------------------------------------------
-! short_ene: calculate the direct space component of the Ewald potentials.
-!            This subroutine was modified by Nathalie Godbout, sgi 04/26/00.
-!            Additional optimizations aimed at IA32 SSE2 were provided by Scott
-!            Brozell, TSRI Oct 2002.  More tweaks were made by Ross Walker,
-!            TSRI 2005.  Heavily modified for msander by DAC, 2019.
-!
-!------------------------------------------------------------------------------
-subroutine short_ene(i, xk, yk, zk, ipairs, ntot, nvdw, nhbnd, eedtbdns, &
-                     eed_cub, eed_lin, charge, ntypes, iac, ico, cn1, cn2, &
-                     cn6, filter_cut, eelt, evdw, force, dir_vir, ee_type, &
-                     eedmeth, dxdr, eedvir, cn3, cn4, cn5)
-  use nblist, only: imagcrds, bckptr, tranvec, cutoffnb, volume
   use constants, only: zero, one, two, half, third, TWOPI, six, twelve
   use file_io_dat
 #ifdef LES
@@ -138,16 +36,30 @@ subroutine short_ene(i, xk, yk, zk, ipairs, ntot, nvdw, nhbnd, eedtbdns, &
   use mbar, only: ifmbar, bar_i, bar_states, bar_lambda, bar_cont, do_mbar
 #endif
   use crg_reloc, only: ifcr, cr_add_dcdr_factor
+  use omp_lib
 
   implicit none
+  character(kind=1, len=13) :: routine="get_nb_energy"
+#ifdef MPI
+#  include "ew_parallel.h"
+#  include "parallel.h"
+#endif
+#include "flocntrl.h"
+#include "def_time.h"
+   
+  integer numatoms, maxnblst, mpoltype
+  integer iac(*), ico(*), ntypes, ee_type, eedmeth
+  _REAL_ charge(*), cn1(*), cn2(*), cn6(*)
+  _REAL_ ewaldcof, eedtbdns, dxdr, eed_cub(*), eed_lin(2,*), dir_vir(3,3)
+  integer ipairs(maxnblst)
+  _REAL_ force(3,numatoms), eelt, epol, evdw, ehb
+  _REAL_ eedvir, filter_cut, dipole(3,*), field(3,*), pol(*)
+  _REAL_ pol2(*)
+  _REAL_ cn3(*), cn4(*), cn5(*)
+   
+  integer index, numpack, i, k, ncell_lo, ncell_hi, ntot, nvdw, nhbnd
   _REAL_ xk, yk, zk
-  integer i, nvdw, nhbnd, ntot
-  integer ipairs(*), ee_type, eedmeth
-  _REAL_ eed_cub(*), eed_lin(2,*), charge(*), dir_vir(3,3), eedvir
-  _REAL_ eedtbdns, filter_cut, dxdr
-  integer ntypes, iac(*), ico(*)
-  _REAL_ cn1(*), cn2(*), cn6(*), eelt, evdw, force(3,*)
-  integer ic,j,m,n,ind,iaci
+  integer ic,j,m,n,ind,iaci,inddel
   _REAL_ del, delrinv, delr12inv
   _REAL_ switch, d_switch_dx
   _REAL_ b0, b1, b2
@@ -158,7 +70,6 @@ subroutine short_ene(i, xk, yk, zk, ipairs, ntot, nvdw, nhbnd, eedtbdns, &
 #ifdef TVDW
   _REAL_ r4, r6pinv
 #endif
-  _REAL_ cn3(*), cn4(*), cn5(*)
   _REAL_ ecur
   integer, parameter :: mask27 = 2**27 - 1
 #ifdef LES
@@ -176,12 +87,70 @@ subroutine short_ene(i, xk, yk, zk, ipairs, ntot, nvdw, nhbnd, eedtbdns, &
   integer itran
 
   _REAL_ time0, time1
-
+   
+  if (do_dir == 0) then
+    return
+  end if
+  eelt = zero
+  epol = zero
+  evdw = zero
+  ehb = zero
+  eedvir = zero
+#ifdef MPI /* SOFT CORE */
+  sc_ener(7) = 0.0d0
+  sc_ener(8) = 0.0d0  
+  sc_ener(9) = 0.0d0
+  sc_ener(10) = 0.d0
+  sc_ener(11) = 0.d0
+#endif
+  dir_vir(1:3,1:3) = zero
   del = one / eedtbdns
+  filter_cut2 = filter_cut * filter_cut
+  numpack = 1
+
+  call timer_start(TIME_SHORT_ENE)
+
+!$omp parallel private( inddel, myindexlo, myindexhi, ncell_lo, ncell_hi, &
+!$omp&  k,i,xk,yk,zk,ntot,nvdw,nhbnd,dumx,dumy,dumz,cgi,iaci,m,xktran, &
+!$omp&  n,itran,j,delx,dely,delz,delr2,delrinv,x,  &
+!$omp&  ind,dx,e3dx,e4dx,switch,d_switch_dx,b0,b1,cgj,comm1,ecur,  &
+!$omp&  dfee,delr2inv,ic,r6,delr12inv,f6,f12,dfx,dfy,dfz)  &
+!$omp&  firstprivate(numpack)
+
+!$  inddel = (nucgrd-1) / OMP_GET_NUM_THREADS() + 1
+!$  if (inddel == 0) inddel = 1
+!$  myindexlo = 1 + OMP_GET_THREAD_NUM()*inddel
+!$  myindexhi = myindexlo + inddel - 1
+!$  if (OMP_GET_THREAD_NUM() == OMP_GET_NUM_THREADS()-1) myindexhi = nucgrd
+
+  ! do index = myindexlo, myindexhi
+  do index = 1,nucgrd
+    if (numimg(index) <= 0) cycle
+    ncell_lo = nlogrid(index)
+    ncell_hi = nhigrid(index)
+    do k = ncell_lo, ncell_hi
+      i = bckptr(k)
+#ifdef MPI /* SOFT CORE */
+      ! SOFT CORE contribution in numsc
+      ntot = numvdw(i) + numhbnd(i) + numsc(i)
+#else
+      ntot = numvdw(i) + numhbnd(i)
+#endif
+      nvdw = numvdw(i)
+      nhbnd = numhbnd(i)
+      if (ntot <= 0) cycle
+!$    if( index < myindexlo .or. index > myindexhi ) then
+!$       ! some other thread will do the work here:
+!$       numpack = numpack + ntot
+!$       cycle
+!$    endif
+      xk = imagcrds(1,k)
+      yk = imagcrds(2,k)
+      zk = imagcrds(3,k)
+!==========  short ene routine in-lined below:===============================
   dumx = zero
   dumy = zero
   dumz = zero
-  filter_cut2 = filter_cut * filter_cut
   cgi = charge(i)
   iaci = ntypes * (iac(i) - 1)
    
@@ -198,15 +167,9 @@ subroutine short_ene(i, xk, yk, zk, ipairs, ntot, nvdw, nhbnd, eedtbdns, &
   !-----------------------------------------------
   ! Loop over the 12-6 LJ and electrostatic terms 
   !-----------------------------------------------
-#if 0   /* following works but slows down execution */
-!$omp parallel do private( n,itran,j,delx,dely,delz,delr2,delrinv,x,  &
-!$omp&  ind,dx,e3dx,e4dx,switch,d_switch_dx,b0,b1,cgj,comm1,ecur,  &
-!$omp&  dfee,delr2inv,ic,r6,f6,f12,dfx,dfy,dfz) &
-!$omp&  reduction(+:eelt,evdw,dumx,dumy,dumz)
-#endif
   do m = 1, nvdw+nhbnd
 
-    n=ipairs(m)
+    n=ipairs(m+numpack-1)
     itran=ishft(n,-27)
     n = iand(n,mask27)
     j = bckptr(n)
@@ -214,6 +177,8 @@ subroutine short_ene(i, xk, yk, zk, ipairs, ntot, nvdw, nhbnd, eedtbdns, &
     dely = imagcrds(2,n) + xktran(2,itran)
     delz = imagcrds(3,n) + xktran(3,itran)
     delr2 = delx*delx + dely*dely+delz*delz
+
+    !  write(6,'(9i5)') index,k,i,j,OMP_GET_THREAD_NUM(),m,n,itran,numpack
  
     if ( delr2 >= filter_cut2 ) cycle
 
@@ -269,12 +234,16 @@ subroutine short_ene(i, xk, yk, zk, ipairs, ntot, nvdw, nhbnd, eedtbdns, &
     end if
 #endif
     ecur = comm1 * b0
+!$omp critical
     eelt = eelt + ecur
+!$omp end critical
 
+#if 0
     ! Thermodynamic Integration decomposition
     if (decpr .and. idecomp > 0) then
       call decpair(2, i, j, ecur/(nstlim/ntpr))
     end if
+#endif
     dfee = comm1*b1
 #ifdef LES
 #  include "ene_decomp.h"
@@ -296,7 +265,9 @@ subroutine short_ene(i, xk, yk, zk, ipairs, ntot, nvdw, nhbnd, eedtbdns, &
        f6 = cn2(ic)*r6
        f12 = cn1(ic)*delr12inv
 #endif
+!$omp critical
        evdw = evdw + f12 - f6
+!$omp end critical
        dfee = dfee + (12.d0*f12 - 6.d0*f6)*delr2inv
     endif
 
@@ -306,199 +277,37 @@ subroutine short_ene(i, xk, yk, zk, ipairs, ntot, nvdw, nhbnd, eedtbdns, &
     dumx = dumx + dfx
     dumy = dumy + dfy
     dumz = dumz + dfz
+!$omp critical
     force(1,j) = force(1,j) + dfx
     force(2,j) = force(2,j) + dfy
     force(3,j) = force(3,j) + dfz
+!$omp end critical
   end do 
-#if 0
-!$omp end parallel do
-#endif
 
   !  end of what is now a single loop over j that are in the list as
   !    being close to i
+
+!   #include "soft_core.inc"   /* MPI code for soft-core electrostatics */
       
-
-#if 0  /*  no longer creating caches needed for the following code: */
-#ifdef MPI /* SOFT CORE */
-
-    ! Now loop over the softcore (modified 12-6) LJ terms for eedmeth = 1
-
-    icount = 0
-
-    ! run over the 3rd subset of the pairlist
-    do m = nvdw+nhbnd+1,ntot      
-#     include "ew_directp.h"
-    end do
-      
-    call vdinvsqrt(icount, cache_r2, cache_df)
-
-    ! cache_r2 is saved for the vdW interactions below
-    cache_r2(1:icount) = dxdr * cache_df(1:icount) * cache_r2(1:icount)
-
-    ! SGI compiler directive to prevent compiler loop fusioning.
-    !*$* NO FUSION
-    do im_new = 1, icount
-      j = cache_bckptr(im_new)
-      delrinv = cache_df(im_new)
-      x = cache_r2(im_new)
-         
-      ! Cubic spline on switch:
-      ind = int(eedtbdns*x)
-      dx = x - ind*del
-      ind = 4*ind
-      e3dx = dx * eed_cub(3+ind)
-      e4dx = dx * dx * eed_cub(4+ind)
-      switch = eed_cub(1+ind) + dx*(eed_cub(2+ind) + (e3dx + e4dx*third)*half)
-      d_switch_dx = eed_cub(2+ind) + e3dx + e4dx*half
-         
-      ! Tom Darden Got the idea for B_l from Walter Smith's CCP5 article 1982
-      ! Ewald for point multipoles
-      cgj = charge(j)
-      comm1 = cgi * cgj
-      delr2inv = delrinv * delrinv
-      if (nsc(i) == nsc(j)) then          
-
-        ! Both atoms are softcore and have normal Coulomb interactions.
-        ! First remove the reciprocal contribution from the main energy array.
-        switch_c = 1.0d0 - switch 
-        eelt = eelt - comm1 * delrinv * switch_c
-        dfee = - comm1 * (switch_c * delrinv + d_switch_dx*dxdr) * delr2inv
-
-        ! Add the full non-switched coulomb pot to the softcore energy array
-        sc_ener(9) = sc_ener(9) + comm1*delrinv
-
-        ! Thermodynamic Integration decomp
-        if (decpr .and. idecomp > 0) then
-          call decpair(2, i, j, -comm1*delrinv*switch_c/(nstlim/ntpr))
-        end if
-
-        ! Scaled up by oneweight
-        dfee = dfee + oneweight * comm1 * delrinv * delr2inv
-        if (ifcr .ne. 0) then
-          b0 = (oneweight - switch_c) * delrinv
-          call cr_add_dcdr_factor(i, b0*cgj)
-          call cr_add_dcdr_factor(j, b0*cgi)
-        end if
-      else 
-
-        ! Use electrostatic softcore potential
-        delr = 1.0d0 / delrinv
-        delr_n = delr**sceeorder
-        if (isProcessV1) then
-
-          ! Atoms are appearing
-          denom = 1.0d0 / (delr_n + scbeta*(1.0d0-clambda))**(1.0d0/sceeorder) 
-          denom_n = denom**(1+sceeorder)
-          b0 = switch * denom
-
-          ! Soft core potential goes into main electrostatic energy
-          eelt = eelt + b0 * comm1
-
-          ! Thermodynamic Integration decomposition
-          if (decpr .and. idecomp > 0) then
-            call decpair(2, i, j, b0*comm1 / (nstlim/ntpr))
-            call decpair(3, i, j, weight1 * switch / sceeorder * comm1 * &
-                         denom_n * scbeta / (nstlim/ntpr))
-          end if
-          sc_dvdl_ee = sc_dvdl_ee + &
-                       switch / sceeorder * comm1 * denom_n * scbeta
-          dfee = -(d_switch_dx*dxdr * comm1 * denom * delrinv) + &
-                  (switch * comm1 * denom_n * delr_n * delr2inv)
-          if (ifcr .ne. 0) then
-            call cr_add_dcdr_factor(i, b0*cgj)
-            call cr_add_dcdr_factor(j, b0*cgi)
-          end if
-
-          ! Collect lambda-dependent contributions to BAR FEP energy 
-          ! differences
-          if (ifmbar .ne. 0 .and. do_mbar) then
-            do bar_i = 1, bar_states
-
-              ! Remove the current lambda cont
-              bar_cont(bar_i) = bar_cont(bar_i) - switch * comm1 * denom
-            end do
-            do bar_i = 1, bar_states
-              denom = 1.0d0 / (delr_n + scbeta * &
-                               (1.0d0 - bar_lambda(bar_i)))**(1.0d0/sceeorder)
-              bar_cont(bar_i) = bar_cont(bar_i) + switch * comm1 * denom
-            end do
-          end if
-        else
-
-          ! Atoms are disappearing
-          denom = 1.0d0 / (delr_n + scbeta * clambda)**(1.0d0/sceeorder)
-          denom_n = denom**(1+sceeorder)
-          b0 = switch * denom
-
-          ! Soft core potential goes into main electrostatic energy
-          eelt = eelt + b0 * comm1
-
-          ! Thermodynamic Integration decomposition
-          if (decpr .and. idecomp > 0) then
-            call decpair(2, i, j, b0*comm1/(nstlim/ntpr))
-            call decpair(3, i, j, weight0 * switch / sceeorder * comm1 * &
-                         denom_n * scbeta / (nstlim/ntpr))
-          end if
-          sc_dvdl_ee = sc_dvdl_ee - &
-                       switch / sceeorder * comm1 * denom_n * scbeta
-          dfee = -(d_switch_dx*dxdr * comm1 * denom * delrinv) + &
-                  (switch * comm1 * denom_n * delr_n * delr2inv)
-          if (ifcr .ne. 0) then
-            call cr_add_dcdr_factor(i, b0*cgj)
-            call cr_add_dcdr_factor(j, b0*cgi)
-          end if
-
-          ! Collect lambda-dependent contributions to
-          ! Bennet Acceptance Ratio Free Energy Perturbation differences
-          if (ifmbar .ne. 0 .and. do_mbar) then
-            do bar_i = 1, bar_states
-
-              ! Remove the current lambda cont
-              bar_cont(bar_i) = bar_cont(bar_i) - switch * comm1 * denom
-            end do
-            do bar_i = 1, bar_states
-              denom = 1.0d0 / (delr_n + &
-                               scbeta * bar_lambda(bar_i))**(1.0d0/sceeorder)
-              bar_cont(bar_i) = bar_cont(bar_i) + switch * comm1 * denom
-            end do
-          end if
-        end if
-      end if
-         
-      ! Inefficient, change this to use another cache later. 
-      cache_r2(im_new) = cache_x(im_new) * cache_x(im_new) + &
-                         cache_y(im_new) * cache_y(im_new) + &
-                         cache_z(im_new) * cache_z(im_new)
-
-      ! The following ew_directe3.h or directe4.h needs r^2 in cache_r2
-      cache_df(im_new) = dfee
-
-    end do
-    ! End epilogue loop
-
-    ! V1 uses ew_directe3.h, in which softcore atoms are treated as appearing,
-    ! i.e. fully interacting at lambda=1 and 'soft' at small lambda.
-    ! V0 uses ew_directe4.h, in which softcore atoms are treated as vanishing, 
-    ! i.e. fully interacting at lambda=0 and 'soft' at large lambda.
-
-    if ( isProcessV1 ) then
-#     include "ew_directe3.h"
-    else
-#     include "ew_directe4.h"
-    end if
-#endif /* MPI for SOFT CORE  */
-#endif  /* if 0 skipping this section */
-
-!   #include "eedmeth2-6.h"
-
   ! Contribute forces on the ith particle, accumulated as forces
   ! from the ith particle were added onto all other particles.
+!$omp critical
   force(1,i) = force(1,i) - dumx
   force(2,i) = force(2,i) - dumy
   force(3,i) = force(3,i) - dumz
+!$omp end critical
+
+!==========  short ene routine in-lined above===============================
+                        
+      numpack = numpack + ntot
+    end do  !  k = ncell_lo,ncell_hi
+  end do
+!$omp end parallel
+  ! nd loop over this process's assigned atoms
+
+  call timer_stop(TIME_SHORT_ENE)
 
   return
 
-end subroutine short_ene 
+end subroutine get_nb_energy 
 
-#include "short_ene_dip.h"
