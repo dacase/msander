e67e22dd6fa277987949747595be5fc3db6de18c
David A Case <david.case@rutgers.edu>
2020-05-25 08:23:26 -0400

    tweak conda package instructions

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,15 +3,7 @@
 export MSANDERHOME=`pwd`
 ./configure 
 
-make install
+make install.ap
 make clean
-
-#if [ `uname` == "Darwin" ]; then
-#    mkdir -p lib/amber_3rd_party # to avoid overwritten by other programs
-#    python ./recipe_ap/copy_and_fix_gfortran.py \
-#        /usr/local/gfortran/lib lib/amber_3rd_party
-#    mkdir -p $PREFIX/lib
-#    rsync -av lib/amber_3rd_party $PREFIX/lib
-#fi
 
 rsync -av bin dat lib $PREFIX
