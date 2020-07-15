57dcb723decf906e1172e97ba0ea8dbb4ec5bdb7
David A Case <david.case@rutgers.edu>
2020-07-15 16:10:04 -0400

    bring recipe here from master

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,12 +6,12 @@ export MSANDERHOME=`pwd`
 make install.ap
 make clean
 
-#if [ `uname` == "Darwin" ]; then
-#    mkdir -p lib/amber_3rd_party # to avoid overwritten by other programs
-#    python ./recipe/copy_and_fix_gfortran.py \
-#        /usr/local/gfortran/lib lib/amber_3rd_party
-#    mkdir -p $PREFIX/lib
-#    rsync -av lib/amber_3rd_party $PREFIX/lib
-#fi
+if [ `uname` == "Darwin" ]; then
+    mkdir -p lib/amber_3rd_party # to avoid overwritten by other programs
+    /Users/case/miniconda/bin/python ./recipe/copy_and_fix_gfortran.py \
+        /usr/local/gfortran/lib lib/amber_3rd_party
+    mkdir -p $PREFIX/lib
+    rsync -av lib/amber_3rd_party $PREFIX/lib
+fi
 
 rsync -av bin dat lib $PREFIX
