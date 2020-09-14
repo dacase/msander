5bf87ca1968c5bc299e473cdd8e3be24a37d6ef6
David A Case <david.case@rutgers.edu>
2020-09-14 16:28:28 -0400

    skip fixing OSX gfortran files

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
 
 export MSANDERHOME=`pwd`
-./configure 
+./configure  --conda --verbose
 
 make install.ap
 make clean
 
+if false; then
 if [ `uname` == "Darwin" ]; then
     mkdir -p lib/amber_3rd_party # to avoid overwritten by other programs
     /Users/case/miniconda/bin/python ./recipe/copy_and_fix_gfortran.py \
         /usr/local/gfortran/lib lib/amber_3rd_party
     mkdir -p $PREFIX/lib
     rsync -av lib/amber_3rd_party $PREFIX/lib
+fi
 fi
 
 rsync -av bin dat lib $PREFIX
