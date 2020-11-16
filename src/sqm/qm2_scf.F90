fbfd91750e748728e240b113bc20acf9891f87a7
David A Case <david.case@rutgers.edu>
2020-11-16 14:21:07 -0500

    set for gcc10; update test cases

--- src/sqm/qm2_scf.F90
+++ src/sqm/qm2_scf.F90
@@ -2253,7 +2253,7 @@ SUBROUTINE SvdInvert_SymMat(n,a,ainv,thresh) ! THRESH,ERR
   _REAL_ :: S(n)
   _REAL_ :: U(n,n)
   _REAL_ :: VT(n,n)
-  _REAL_ :: twork
+  _REAL_ :: twork(1)
   _REAL_,ALLOCATABLE :: WORK(:)
   INTEGER :: LWORK
 
@@ -2268,7 +2268,7 @@ SUBROUTINE SvdInvert_SymMat(n,a,ainv,thresh) ! THRESH,ERR
   LWORK = -1
   CALL DGESVD("A","A",n,n, ainv,n, S,U,n, VT,n, twork,LWORK,my_err)
   
-  LWORK = NINT(twork)
+  LWORK = NINT(twork(1))
   ALLOCATE( WORK(LWORK) )
   WORK = 0.d0
   
