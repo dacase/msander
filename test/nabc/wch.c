ca5363dbd3def5b8204a236ce2057075c3574696
David A Case <david.case@rutgers.edu>
2020-12-30 20:47:53 -0500

    add tests for fd_helix and wc_helix

---
+++ test/nabc/wch.c
@@ -0,0 +1,14 @@
+
+//  simple driver script to test:
+
+#include "nabc.h"
+
+int main(){
+
+    MOLECULE_T  *m;
+    char  *seq =  "ggcaattcgc";
+    char  *cseq = "ccgttaagcg";
+
+    m = wc_helix( seq, "dna", cseq, "dna", 2.25, -4.96, 36.0, 3.38, "s5a5s3a3");
+    putpdb( "wch.pdb", m, NULL );
+}
