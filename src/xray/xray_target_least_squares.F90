9e03887ebb590bee1dbd388fafca4e205877a6b8
David A Case <david.case@rutgers.edu>
2022-09-22 11:27:03 -0400

    make ls target optimize an Rwork-like function

--- src/xray/xray_target_least_squares.F90
+++ src/xray/xray_target_least_squares.F90
@@ -36,6 +36,57 @@ contains
         real(real_kind), intent(out), optional :: deriv(size(absFobs))
         real(real_kind), intent(out), optional :: xray_energy
 
+        real(real_kind) :: e,rij,r1,dif1,r2,k2,df,r3,k3,r4,dif
+        integer :: i
+
+#if 1   /* optimize R_work, based on disnrg()  */
+        deriv(n_work + 1:) = 0 ! no force for things unselected here
+        xray_energy = 0
+
+        ! initial gueses:
+        r1 = -0.3
+        r2 = -0.1
+        r3 =  0.1
+        r4 =  0.3
+        k2 =  1.0
+        k3 =  1.0
+
+        do i = 1,n_work
+           rij = absFcalc(i) - absFobs(i)
+           if (rij < r1) then
+               dif1 = r1-r2
+               df = 2.0d0 * k2 * dif1
+               e = df * (rij-r1) + k2*dif1*dif1
+            else if (rij < r2) then
+               dif = rij - r2
+               df = 2.0d0 * k2 * dif
+               e = k2*dif*dif
+            else if (rij <= r3) then
+               df = 0.d0
+               e = 0.0d0
+            else if (rij < r4) then
+               dif = rij - r3
+               df = 2.0d0 * k3 * dif
+               e = k3*dif*dif
+            else
+               dif1 = r4-r3
+               df = 2.0d0 * k3 * dif1
+               e = df * (rij-r4) + k3*dif1*dif1
+            end if
+            deriv(i) = df
+            xray_energy = xray_energy + e
+        end do
+
+        ! if (present(deriv)) then
+        !     deriv(n_work + 1:) = 0 ! no force for things unselected here
+        !     deriv(:n_work) = sign(absFcalc(:n_work), &
+        !        absFcalc(:n_work) - absFobs(:n_work)) / denom
+        ! endif
+        ! if (present(xray_energy))then
+        !     xray_energy = sum( abs(absFobs(:n_work) - absFcalc(:n_work)))
+        ! end if
+
+#else  /* weighted least-squares  */
         if (present(deriv)) then
             deriv(n_work + 1:) = 0 ! no force for things unselected here
             deriv(:n_work) = 2.d0 * sigma_Fobs(:n_work) * &
@@ -45,6 +96,8 @@ contains
             xray_energy = sum( sigma_Fobs(:n_work) * &
                (absFobs(:n_work) - absFcalc(:n_work))**2)
         end if
+#endif
+
     end subroutine calc_partial_d_target_d_absFcalc
 
 end module xray_target_least_squares_module
